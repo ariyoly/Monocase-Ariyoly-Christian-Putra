33ca56962a68f32be509a2b558bdbb24d53c9e15
Ariyoly <tianp6211@gmail.com>
2024-12-07 20:16:26 +0700

    update2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Check } from "lucide-react";
-import Image from "next/image";
-
+ 
 export default function Home() {
   return (
     <div className="bg-slate-50">
@@ -11,7 +10,7 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
-                <img src="/logo-monocase.png" className="w-full"/>
+                <img src="\logo-monocase.png" className="w-full"/>
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5x1 md:text-6xl lg:text-7xl">Your Image on a <span className="bg-orange-500 px-2 text-white">Custom</span> Phone Case</h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">Capture your favorite memories with your own, <span className="font-semibold">one-of-one</span> phone case. Monocase allows you to protect your memories, not just your phone case.</p>
@@ -26,10 +25,25 @@ export default function Home() {
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
                     <Check className="h-5 w-5 shrink-0 text-green-500"/>
-                    5 year print guaranteehbhb
+                    5 year print guaarantee
+                  </li>
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-500"/>
+                    Modern Iphone models support
                   </li>
                 </div>
               </ul>
+
+
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="\person-1.png" alt="user profile" />
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="\person-2.png" alt="user profile" />
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="\person-3.png" alt="user profile" />
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="\person-4.png" alt="user profile" />
+                  <img className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" src="\person-5.png" alt="user profile" />
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
