f1d39bdb8cbbcaa25f32ab6d0cfe7a1d9b19ca02
Ariyoly <tianp6211@gmail.com>
2024-12-10 20:18:28 +0700

    update lagi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+import { Icons } from "@/components/icon";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
-import { Check, Star } from "lucide-react";
+import { Check, Icon, Star } from "lucide-react";
  
 
 
@@ -78,8 +79,18 @@ export default function Home() {
         </MaxWidthWrapper>
       </section>
 
+      
+      {/* {value proposition section} */}
+      <section className="bg-slate-100">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">What our <span className="relative px-2">customers <Icons.Underline/> </span> say</h2>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
     </div>
   );
 }
 
-// 59:08 stop 
+// 1:10:08 stop 
