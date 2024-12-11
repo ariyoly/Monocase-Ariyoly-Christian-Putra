feb50ea71ab835eff66adb41b733943a7679d6d0
Ariyoly <tianp6211@gmail.com>
2024-12-11 13:55:14 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,10 +81,67 @@ export default function Home() {
 
       
       {/* {value proposition section} */}
-      <section className="bg-slate-100">
+      <section className="bg-slate-50">
         <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
           <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
-            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">What our <span className="relative px-2">customers <Icons.Underline/> </span> say</h2>
+            <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">What our <span className="relative px-2">customers <Icons.Underline className="hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-violet-500"/></span>{' '} say</h2>
+
+
+            <img src="/Logo Monocase 2.png" className="w-24 order-0 lg:order-2"/>
+          </div>
+
+
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:*max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-violet-500 fill-violet-500"/>
+                <Star className="h-5 w-5 text-violet-500 fill-violet-500"/>
+                <Star className="h-5 w-5 text-violet-500 fill-violet-500"/>
+                <Star className="h-5 w-5 text-violet-500 fill-violet-500"/>
+                <Star className="h-5 w-5 text-violet-500 fill-violet-500"/>
+              </div>
+              <div className="text-lg leading-8">
+                <p>"The case feels durable and I even got a compliment on the design. Had the case for two and half monts now and <span className="p-0.5 bg-slate-600 text-white">the image is super clear</span>, on the case i had before, the image started fading into yellowing color after couple week."</p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img className="rounded-full h-12 w-12 object-cover" src="/person-1.png" alt="user"/>  
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-500">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-500"/>
+                    <p className="text-sm">Verified purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            {/* second user */}
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-violet-500 fill-violet-500"/>
+                <Star className="h-5 w-5 text-violet-500 fill-violet-500"/>
+                <Star className="h-5 w-5 text-violet-500 fill-violet-500"/>
+                <Star className="h-5 w-5 text-violet-500 fill-violet-500"/>
+                <Star className="h-5 w-5 text-violet-500 fill-violet-500"/>
+              </div>
+              <div className="text-lg leading-8">
+                <p>"The case feels durable and I even got a compliment on the design. Had the case for two and half monts now and <span className="p-0.5 bg-slate-600 text-white">the image is super clear</span>, on the case i had before, the image started fading into yellowing color after couple week."</p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img className="rounded-full h-12 w-12 object-cover" src="/person-1.png" alt="user"/>  
+                <div className="flex flex-col">
+                  <p className="font-semibold">Jonathan</p>
+                  <div className="flex gap-1.5 items-center text-zinc-500">
+                    <Check className="h-4 w-4 stroke-[3px] text-green-500"/>
+                    <p className="text-sm">Verified purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+
           </div>
         </MaxWidthWrapper>
       </section>
