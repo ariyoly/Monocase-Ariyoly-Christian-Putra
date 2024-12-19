77a9fa0f374d1d6e86c7f52cd8479f5f67242eed
Ariyoly <tianp6211@gmail.com>
2024-12-19 20:49:28 +0700

    updateee

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,5 +1,13 @@
-const Page = () => {
-    return <p className=""></p>
+interface PageProps{
+    searchParams: {
+        [key: string]: string | string[] | undefined
+    }
+}
+
+
+const Page = ({searchParams}: PageProps) => {
+    const {id} = searchParams
+    return <p>{id}</p>
 }
 
 export default Page 
